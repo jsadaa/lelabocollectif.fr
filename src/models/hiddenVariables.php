fbb1536ac5d20b87ea0a1a9ba44e7f9db030a790
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2022-05-30 17:16:45 +0200

    Delete hiddenVariables.php

--- src/models/hiddenVariables.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$sqlConfig = 'mysql:host=db5007429476.hosting-data.io;dbname=dbs6121931;charset=utf8';
-$sqlId = 'dbu2184153';
-$sqlPassword = 'High940521071989!';
-$gmailAccount = 'lelabocollectif@gmail.com';
-$gmailPassword = 'High940521071989!';
